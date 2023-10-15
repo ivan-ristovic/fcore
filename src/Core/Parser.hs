04cdeba1a0e6eea1f3d6094aa5bc4c76b69d2f12
ivan-ristovic <work@ristovic.net>
2023-10-15 20:48:31 +0200

    [parser] add first parser version

---
+++ src/Core/Parser.hs
@@ -0,0 +1,122 @@
+module Core.Parser 
+    ( ParseError
+    , parseCoreFile
+    , parseCoreProgram
+    ) where
+
+import Control.Exception (Exception(), throw)
+import Data.Functor
+import Text.Parsec hiding (spaces)
+import Text.Parsec.Language
+import qualified Text.Parsec.Token as PT
+import qualified Text.Parsec.Expr as P
+import Text.Parsec.String (Parser())
+
+import Core.Language
+
+
+instance Exception ParseError
+
+parseCoreFile :: FilePath -> IO CoreProgram
+parseCoreFile f = do
+    contents <- readFile f
+    case invokeParser f contents of 
+        Left  err  -> throw err
+        Right prog -> return prog
+
+parseCoreProgram :: String -> Either ParseError CoreProgram
+parseCoreProgram = invokeParser ""
+
+invokeParser :: SourceName -> String -> Either ParseError CoreProgram
+invokeParser = parse pProgram
+
+
+coreStyle = haskellStyle { PT.reservedNames   = keywords
+                         , PT.reservedOpNames = reservedOps
+                         }
+coreLexer  = PT.makeTokenParser coreStyle
+parens     = PT.parens coreLexer
+braces     = PT.braces coreLexer
+identifier = PT.identifier coreLexer
+reserved   = PT.reserved coreLexer
+reservedOp = PT.reservedOp coreLexer
+integer    = PT.integer coreLexer
+int        = read <$> many1 digit
+symbol     = PT.symbol coreLexer
+operator   = PT.operator coreLexer
+spaces     = PT.whiteSpace coreLexer
+semi       = PT.semi coreLexer
+
+binary op = P.Infix e P.AssocLeft 
+    where e = do o <- symbol op
+                 return (\x y -> EAp (EAp (EVar o) x) y)
+
+sequence1 :: Parser a -> Parser [a]
+sequence1 = PT.semiSep1 coreLexer
+
+pProgram :: Parser CoreProgram
+pProgram = spaces *> sequence1 pScDef
+
+pScDef :: Parser CoreScDef
+pScDef = do
+    name <- identifier
+    args <- many identifier
+    reservedOp opAssign
+    body <- pExpr
+    return (name, args, body)
+
+pExpr :: Parser CoreExpr
+pExpr = choice [pLet, pCase, pLam, expr1] 
+  where 
+    expr1 = P.buildExpressionParser table term
+    table = [ map binary [opAmul, opAdiv]
+            , map binary [opAadd, opAsub]
+            , map binary relationalOps
+            , [ binary opLand ]
+            , [ binary opLor ]
+            ]
+
+    term = foldl1 EAp <$> many1 pAexpr
+
+    pLam = do
+        reservedOp opLambda
+        params <- many1 identifier
+        reservedOp opRArrow
+        expr <- pExpr
+        return (ELam params expr)
+
+    pCase = do
+        reserved kwCase
+        e <- pExpr
+        reserved kwOf
+        alts <- sequence1 pAlt1
+        return (ECase e alts)
+        where
+          pAlt1 = do
+              i <- reservedOp opLBrace *> int <* reservedOp opRBrace
+              vars <- many identifier
+              reservedOp opRArrow
+              expr <- pExpr
+              return (i, vars, expr)
+
+    pLet  = do
+        isrec <- try (reserved kwLetNRec $> False) <|> (reserved kwLetRec $> True)
+        binds <- sequence1 (try bind1)
+        reserved kwIn
+        expr <- pExpr
+        return (ELet isrec binds expr)
+        where
+          bind1 = do
+              name <- identifier
+              reservedOp opAssign
+              expr <- pExpr
+              return (name, expr)
+
+    pCtor = do
+        reserved kwCons
+        braces $ ECons <$> int <* reservedOp opComma <*> int
+
+    pAexpr = spaces *> choice [pVar, pNum, pCtor, parens pExpr] <* spaces
+
+    pNum  = ENum <$> integer
+    pVar  = EVar <$> identifier
--- test/Core/Tests/Parser.hs
+++ test/Core/Tests/Parser.hs
@@ -3,6 +3,61 @@ module Core.Tests.Parser (parserTests) where
 import Test.Tasty
 import Test.Tasty.HUnit
 
+import Core.Language
+import Core.Parser
+
+testPrefix :: String
+testPrefix = "Tests.Parser."
+
 parserTests :: [TestTree]
-parserTests = []
+parserTests = [ test_sample_I
+              , test_sample_id
+              , test_sample_id_twice
+              , test_sample_updating
+              , test_sample_arithm
+              ] 
+
+src_dir = "samples/"
+
+testParseFile :: FilePath -> CoreProgram -> Assertion
+testParseFile path expected = do
+    actual <- parseCoreFile path
+    assertEqual ("smoke test for " ++ path) expected actual
+
+test_sample_I :: TestTree
+test_sample_I = testCase (testPrefix ++ src)
+    (testParseFile src    
+        [ ("main", [], EAp (EVar "I") (ENum 3)) ] 
+    ) where src = src_dir ++ "i.fc"
+        
+test_sample_id :: TestTree
+test_sample_id = testCase (testPrefix ++ src)
+    (testParseFile src
+        [ ("id"  , [], EAp (EAp (EVar "S") (EVar "K")) (EVar "K"))
+        , ("main", [], EAp (EVar "id") (ENum 3))
+        ]
+    ) where src = src_dir ++ "id.fc"
+    
+test_sample_id_twice :: TestTree
+test_sample_id_twice = testCase (testPrefix ++ src)
+    (testParseFile src
+        [ ("id"  , [], EAp (EAp (EVar "S") (EVar "K")) (EVar "K"))
+        , ("main", [], EAp (EAp (EAp (EAp (EVar "twice") (EVar "twice")) (EVar "twice")) (EVar "id")) (ENum 3))
+        ] 
+    ) where src = src_dir ++ "id_twice.fc"
+
+test_sample_updating :: TestTree
+test_sample_updating = testCase (testPrefix ++ src)
+    (testParseFile src
+        [ ("main", [], EAp (EAp (EVar "twice") (EAp (EAp (EVar "I") (EVar "I")) (EVar "I"))) (ENum 3)) ] 
+    ) where src = src_dir ++ "updating.fc"
+
+test_sample_arithm :: TestTree
+test_sample_arithm = testCase (testPrefix ++ src)
+    (testParseFile src
+        [ ("main", [], EAp (EAp (EVar "twice") (EAp (EAp (EVar "I") (EVar "I")) (EVar "I"))) (ENum 3)) ] 
+    ) where src = src_dir ++ "arithm.fc"
+
+
+
 
